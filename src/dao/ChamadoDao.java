a82d5ca83f676c33b210140b36298f6d00e3ec9c
Lays Maiara <lays.devstudio@gmail.com>
2022-03-27 19:21:26 -0300

    Segunda entrega - POO

---
+++ src/dao/ChamadoDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Chamado;
+
+public class ChamadoDao {
+
+	private static ChamadoDao instance;
+	private List<Chamado> listaChamados = new ArrayList<>();
+
+	public static ChamadoDao getInstance() {
+		if (instance == null) {
+			instance = new ChamadoDao();
+		}
+		return instance;
+	}
+
+	public void registrarChamado(Chamado chamado) {
+		listaChamados.add(chamado);
+	}
+}
